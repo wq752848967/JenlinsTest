0c08ca86737d16f5d4d817fbdfc9273fc7ab0f96
wq752848967 <752848967@qq.com>
2017-09-09 22:51:47 +0800

    add unittest file

---
+++ pytest/test_unittest.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+import unittest
+from sparking import Sparking
+
+class mytest(unittest.TestCase):
+  ##初始化工作
+  def setUp(self):
+    pass
+  #退出清理工作
+  def tearDown(self):
+    pass
+  #具体的测试用例，一定要以test开头
+  def testsum(self):
+    self.assertEqual(Sparking.get_num_of_one(8,5), 3, 'test sum fail')
+
+
+if __name__ =='__main__':
+  unittest.main()
